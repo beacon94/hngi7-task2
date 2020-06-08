e15dc41cf534ca4c50f9fda47d8680bdbe12b587
beacon94 <66219098+beacon94@users.noreply.github.com>
2020-06-08 10:45:30 +0100

    Create Beacon.py

---
+++ Beacon.py
@@ -0,0 +1,7 @@
+full_name = "Aworeni Ajibola"
+hng_id = "HNG-00235"
+email = "Amaworeniajibola@gmail.com"
+language = "python"
+string = "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task email{}"
+print(string.fromat(email, full_name, hng_id, language))
+© 2020 GitHub, Inc.
